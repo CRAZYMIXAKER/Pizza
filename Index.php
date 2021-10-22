bb1ad810361606150b8c3dc770d4bd132d55ecdf
CRAZYMIXAKER <zombisuki@gmail.com>
2021-10-22 14:00:10 +0300

    FirstCommit

---
+++ Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Pizza</title>
+	<link rel="shortcut icon" href="./pizza.png" type="image/png" />
+	<link rel="stylesheet" href="./main.css" />
+</head>
+
+<body>
+	<h2>Pizza</h2>
+</body>
+
+</html>
